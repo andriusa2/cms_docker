4a3984ca8e304263f13517c53dcacf6a8f69ef90
Andrius Žiūkas <andriusa2@gmail.com>
2024-02-27 06:25:35 +0200

    Add local install scripts

---
+++ aws/scripts/99_local_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Mirrors packer_conf and sets local machine as contest one.
+set -eux
+
+# Precreate tmp dirs
+mkdir -p /tmp/{cms,cms-misc-bin,cms-misc-service,nginx,supervisor,www}
+# Copy the files to expected tmp places
+cp -r rootdir/usr/local/etc/* /tmp/cms/
+cp -r send_logs/cmsSendLogs /tmp/cms-misc-bin/
+cp -r send_logs/cms-send-logs.service /tmp/cms-misc-service/
+cp -r rootdir/etc/nginx/* /tmp/nginx/
+cp -r rootdir/etc/supervisor/* /tmp/supervisor/
+cp -r rootdir/var/www/* /tmp/www/
+cp dejavu-fonts-ttf-2.37.zip /tmp/
+cp html-book-20230810.zip /tmp/
+/bin/sh scripts/00_common_deps.sh
+/bin/sh scripts/01_pyenv.sh
+/bin/sh scripts/02_cms.sh
+/bin/sh scripts/03_configs_and_webserving.sh
+/bin/sh scripts/04_helper_binaries.sh
