656c7e8ed7151af01086b764d09fc7ce2c86f80c
Andrius Žiūkas <andriusa2@gmail.com>
2025-11-01 22:47:32 +0000

    Use pyenv shims for supervisor configs.
    These should work and not require ugly config manipulation.

--- aws/scripts/02_cms.sh
+++ aws/scripts/02_cms.sh
@@ -74,9 +74,3 @@ CMS_EGG_PATH=$( pip3 show cms | grep Location: | cut -f2 -d' ' )
 sudo ln -s "${CMS_EGG_PATH}/cms/server/contest/static" "/var/www/cws_static"
 sudo ln -s "${CMS_EGG_PATH}/cms/server/admin/static" "/var/www/aws_static"
 sudo ln -s "${CMS_EGG_PATH}/cms/server/static" "/var/www/cms_static"
-
-# Link binaries to /usr/local/bin
-for bin in "${VIRTUAL_ENV}"/bin/cms*
-do
-  sudo ln -s "${bin}" /usr/local/bin/
-done
